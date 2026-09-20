7cb76a7e82e647928087b5ebaf524037bc4942bc
Diaa Aldin Drak Alsebai <diaa.alsebai.dev@gmail.com>
2026-09-21 00:56:07 +0300

    added views and urls to tasks app

--- l4/tasks/views.py
+++ l4/tasks/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Task
+
+from .forms import TaskForm
 
 # Create your views here.
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def task_details(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'tasks/task_details.html', {'task': task})
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect('tasks:task_details', pk=task.pk)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form, 'mode': 'create'})
+
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:task_details', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form, 'mode': 'edit'})
+
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if (request.method == 'POST'):
+        task.delete()
+        return redirect('tasks:task_list')
+    return render(request, 'tasks/task_delete_confirmation.html', {'task': task})
