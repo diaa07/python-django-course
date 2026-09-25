78ae163efcebfde1af1ee2421282cc04a386efc1
Diaa Aldin Drak Alsebai <diaa.alsebai.dev@gmail.com>
2026-09-25 15:05:40 +0300

    project(p1)-feat(common): created abstract models for timestamps and slugs

---
+++ p1/common/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.text import slugify
+
+import uuid
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SluggedModel (models.Model):
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    slug_source = "name"
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if (not self.slug):
+            source_value = getattr(self, self.slug_source, "")
+            base_slug = (slugify(source_value, allow_unicode=True)
+                         or "item")[:241]
+            base_uuid = str(uuid.uuid4())[:8]
+            self.slug = f"{base_slug}-{base_uuid}"
+
+        super().save(*args, **kwargs)
